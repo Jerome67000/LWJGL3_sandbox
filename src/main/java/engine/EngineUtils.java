86b9190ad0ddd8776fa15a02285ff301f456287b
Jerome67000 <jerome.gully0@gmail.com>
2017-05-30 15:07:43 +0200

    fix resources loader

--- src/main/java/engine/EngineUtils.java
+++ src/main/java/engine/EngineUtils.java
@@ -1,5 +1,8 @@
 package engine;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
 
@@ -8,12 +11,18 @@ import java.util.Scanner;
  */
 public class EngineUtils {
 
-    public static String loadRessource(String fileName) throws Exception {
-        String result;
-        try (InputStream in = EngineUtils.class.getClass().getResourceAsStream(fileName);
-             Scanner scanner = new Scanner(in, "UTF-8")) {
+    public static String loadResource(String fileName) throws Exception {
+        String result = "";
+        try (InputStream in = EngineUtils.class.getClass().getResourceAsStream(fileName)) {
+            Scanner scanner = new Scanner(in, "UTF-8");
             result = scanner.useDelimiter("\\A").next();
+        } catch (Exception e) {
+            System.err.println("Can't load resource: " + fileName);
         }
+
+        System.out.println("****** Resource loaded: " + fileName +  " \n" + result + "\n");
+
         return result;
     }
+
 }

1d086c24d3c124fb4d1dccb290517ebbc6a538fb
Jerome67000 <jerome.gully0@gmail.com>
2017-06-11 23:40:34 +0200

    Add utils method to read all lines of a file (OBJLoader)

--- src/main/java/engine/EngineUtils.java
+++ src/main/java/engine/EngineUtils.java
@@ -1,14 +1,14 @@
 package engine;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-/**
- * Created by Jerome on 29/05/2017.
- */
+
 public class EngineUtils {
 
     public static String loadResource(String fileName) throws Exception {
@@ -25,4 +25,22 @@ public class EngineUtils {
         return result;
     }
 
+    public static List<String> readAllLines(String fileName) {
+        List<String> list = new ArrayList<>();
+
+        InputStream in = EngineUtils.class.getResourceAsStream(fileName);
+        InputStreamReader streamReader = new InputStreamReader(in);
+        BufferedReader reader = new BufferedReader(streamReader);
+
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
 }
